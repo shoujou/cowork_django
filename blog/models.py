3e10e4c332214a8a50185670ce98651a84cbc20a
xpro-52 <xpro46720@gmail.com>
2022-04-22 21:03:09 +0900

    model 0

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(_("name"), max_length=150)
+    user = models.OneToOneField(to=get_user_model(), 
+                                verbose_name=_("user"), 
+                                on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(_("title"), max_length=150)
+    body = models.TextField(_("body"))
+    created_date = models.DateTimeField(_("created date"), default=timezone.now)
+    author = models.ForeignKey(to=Author,
+                               verbose_name=_("author"), 
+                               on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
