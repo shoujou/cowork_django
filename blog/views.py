34a4475b48a8fc6cb39c78896539d78a2aad5f3f
xpro-52 <xpro46720@gmail.com>
2022-04-22 22:00:48 +0900

    views 0

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from blog.models import Post
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
+
+
+class PostListView(generic.ListView):
+    template_name = "blog/post_list.html"
+    context_object_name = "post_list"
+    model = Post
+
+
+class PostCreateView(generic.CreateView):
+    pass
